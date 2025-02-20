19e6f8c9c4e86eccd2874fd7ea5e0137c7ef6d50
locjdev <voduyloc.dev@gmail.com>
2025-02-21 02:15:15 +0700

    first commit

---
+++ src/test/java/com/lencraft/aws_s3_service/AwsS3ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lencraft.aws_s3_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
